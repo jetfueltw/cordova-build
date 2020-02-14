5a08dd4ce56b58bb68e9b90f700e3f80bcedfbea
richguo <richguo@jetfuel.com.tw>
2020-02-14 17:35:38 +0800

    update: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,3 @@
 docker tag jetfueltw/cordova:android-28 jetfueltw/cordova:latest
 docker push jetfueltw/cordova:android-28
 docker push jetfueltw/cordova:latest
-
-docker tag jetfueltw/cordova:android-28 richguo0615dk/cordova:latest
-docker push richguo0615dk/cordova:latest
